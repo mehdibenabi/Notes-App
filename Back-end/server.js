c4e5a4e1ebcffcd359183462683c632b7974d82a
Mehdi Benabi <mehdibennabi68@gmail.com>
2024-07-25 13:03:05 +0000

    ..

--- Back-end/server.js
+++ Back-end/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://notes-app-front-sable.vercel.app",
+  origin: ('*'),
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
